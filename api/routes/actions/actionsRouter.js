8cffce5c79265a1a363deb4a4f7d58bb45fd584e
ndurbin <nick.durbin@gmail.com>
2019-12-14 11:21:17 -0500

    Action routes created with CRUD functionality.

---
+++ api/routes/actions/actionsRouter.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const actions = require('../../data/helpers/actionModel');
+const { validateAction, validateActionId } = require('../../middleware/');
+const router = express.Router({ mergeParams: true });
+
+router.get("/", (req, res, next) => {
+  actions.get()
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+router.get("/:id", validateActionId, (req, res, next) => {
+  actions.get(req.params.id)
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+router.post("/:id", validateActionId, validateAction, (req, res, next) => {
+  const { id } = req.params
+  const { description, notes } = req.body
+
+  actions.insert({ project_id: id, description, notes })
+    .then(action => {
+      res.status(201).json(action)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+router.put("/:id", validateActionId, validateAction, (req, res, next) => {
+  const { id } = req.params
+  const { description, notes } = req.body
+
+  actions.update(id, { description, notes })
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+router.delete("/:id", validateActionId, (req, res, next) => {
+  const { id } = req.params
+
+  actions.remove(id)
+    .then(action => {
+      return res.status(200).json(action)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+module.exports = router;
